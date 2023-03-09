3c4f943593c34599a1cc4e2a4b925e601b59f166
Harbir Singh <harbir066@gmail.com>
2023-03-09 14:02:35 +0530

    chore : update database configs

--- database/config/config.js
+++ database/config/config.js
@@ -1,20 +1,18 @@
-const dotenv = require('dotenv');
-dotenv.config();
 module.exports = {
   development: {
     username: 'postgres',
     password: 'password',
     database: 'authorization',
-    host: '127.0.0.1',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
-    port: 5442,
+    port: process.env.DB_PORT || 5442,
   },
   test: {
     username: 'postgres',
     password: 'password',
     database: 'database_test',
-    host: '127.0.0.1',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'post',
-    port: 5442,
+    port: process.env.DB_PORT || 5442,
   },
 };
